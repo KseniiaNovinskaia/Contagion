aece160cfd8fc66c6b46fe5d348292f54c5ccce8
Kimon Haars <kimon.haars@gmail.com>
2021-01-26 20:47:51 +0100

    finalize update and infection_params add one infection instance

--- app/controllers/infections_controller.rb
+++ app/controllers/infections_controller.rb
@@ -6,12 +6,16 @@ class InfectionsController < ApplicationController
   def update
     @infection = Infection.find(params[:id])
     @infection.update(infection_params)
+    redirect_to organisms_path # this can later be changed so that the redirect
+    #leads to the dashboard or something
   end
 
   private
   # We will probably have a merge conflict here, but that's fine, we'll just
   # keep one of the two versions. I need to create it so that I can test my stuff
   def infection_params
-    params.require(:infection).permit()
+    params.require(:infection).permit(:status)
+    # I might have to add the organsim_id or something here. There was something
+    # about that in our last project I think.
   end
 end
