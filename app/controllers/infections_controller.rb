98fe5a0ab7ea5d30182a0eda8fbc5020fd55e043
Kimon Haars <kimon.haars@gmail.com>
2021-01-26 20:30:47 +0100

    edit and update in raw form

--- app/controllers/infections_controller.rb
+++ app/controllers/infections_controller.rb
@@ -1,2 +1,17 @@
 class InfectionsController < ApplicationController
+  def edit
+    @infection = Infection.find(params[:id])
+  end
+
+  def update
+    @infection = Infection.find(params[:id])
+    @infection.update(infection_params)
+  end
+
+  private
+  # We will probably have a merge conflict here, but that's fine, we'll just
+  # keep one of the two versions. I need to create it so that I can test my stuff
+  def infection_params
+    params.require(:infection).permit()
+  end
 end
