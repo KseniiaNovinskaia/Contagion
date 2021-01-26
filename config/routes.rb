a530ee8b7e30d5218b566ccdb39714236819e3d8
Kseniia Novinskaia <kseniia.novinskaia@gmail.com>
2021-01-26 19:12:41 +0100

    Add routes for teh app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :organisms, only: [:index, :show] do
+    resources :infections, only: [:new, :create, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
