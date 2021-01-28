4100990f0586bc23ab96acbbaa46e04d867c3eec
Kseniia Novinskaia <kseniia.novinskaia@gmail.com>
2021-01-28 08:13:18 +0100

    add create action to infections controller

--- app/controllers/infections_controller.rb
+++ app/controllers/infections_controller.rb
@@ -1,2 +1,22 @@
 class InfectionsController < ApplicationController
+
+  # def new
+  # end
+
+  def create
+    @organism = Organism.find(params[:organism_id])
+    @infection = Infection.new(infection_params)
+    @infection.organism = @organism
+    if @infection.save
+      redirect_to organism_path(@organism)
+    else
+      render "infections/show"
+    end
+  end
+
+  private
+
+  def infection_params
+    params.require(:infection).permit(:infection_start, :infection_end)
+  end
 end
