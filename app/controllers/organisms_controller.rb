a2f9d0c63b283cf96563700322d2a2a318b022f5
Philipp Schaefer <pschaefer@posteo.net>
2021-02-01 18:52:30 +0100

    cleaned up code

--- app/controllers/organisms_controller.rb
+++ app/controllers/organisms_controller.rb
@@ -1,11 +1,7 @@
 class OrganismsController < ApplicationController
   def index
-    # init_species
-    # init_body_temperature
-    # init_age
     initialize_search
     handle_search_name
-    # raise
   end
 
   def show
@@ -26,44 +22,14 @@ class OrganismsController < ApplicationController
   end
 
   def handle_search_name
-    name = session[:search_name]
+    # name = session[:search_name]
     species = session[:search_species]
     body_temperature = session[:search_body_temperature]
     age = session[:search_age]
     @organisms = Organism.all
-    @organisms = @organisms.search_by_name(name) unless name.blank?
+    # @organisms = @organisms.search_by_name(name) unless name.blank?
     @organisms = @organisms.select { |organism| species.include?(organism.species) } unless species.blank?
     @organisms = @organisms.select { |organism| organism.body_temperature <= body_temperature.to_i } unless body_temperature.blank?
     @organisms = @organisms.select { |organism| organism.age <= age.to_i } unless age.blank?
   end
-
-  # def init_species
-  #   @species = []
-  #   Organism.all.each do |organism|
-  #     @species << organism.species unless @species.include?(organism.species)
-  #   end
-  #   @species.sort!
-  # end
-
-  # def init_body_temperature
-  #   body_temperatures = []
-  #   Organism.all.each do |organism|
-  #     body_temperatures << organism.body_temperature unless body_temperatures.include?(organism.body_temperature)
-  #   end
-  #   body_temperatures.sort!
-  #   @min_body_temperature = body_temperatures.first
-  #   @max_body_temperature = body_temperatures.last
-  #   @mid_body_temperature = (@min_body_temperature + @max_body_temperature) / 2
-  # end
-
-  # def init_age
-  #   ages = []
-  #   Organism.all.each do |organism|
-  #     ages << organism.age unless ages.include?(organism.age)
-  #   end
-  #   ages.sort!
-  #   @min_age = ages.first
-  #   @max_age = ages.last
-  #   @mid_age = (@min_age + @max_age) / 2
-  # end
 end
--- app/models/organism.rb
+++ app/models/organism.rb
@@ -4,22 +4,10 @@ class Organism < ApplicationRecord
 
   validates :name, presence: true
 
-  include PgSearch::Model
-  pg_search_scope :search_by_name,
-                  against: :name,
-                  using: {
-                    tsearch: { prefix: true }
-                  }
-
-  pg_search_scope :search_by_species,
-                  against: :species
-                  # using: {
-                  #   tsearch: { prefix: true }
-                  # }
-
-  pg_search_scope :search_by_body_temperature,
-                  against: :body_temperature
-                  # using: {
-                  #   tsearch: { prefix: true }
-                  # }
+  # include PgSearch::Model
+  # pg_search_scope :search_by_name,
+  #                 against: :name,
+  #                 using: {
+  #                   tsearch: { prefix: true }
+  #                 }
 end
